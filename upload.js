bc555be385394af40a42b209a2e50aaf3f6142b5
Lars Kiesow <lkiesow@uos.de>
2016-10-02 17:55:30 +0200

    enc and dec test

--- upload.js
+++ upload.js
@@ -1,28 +1,86 @@
+var JsonFormatter = {
+	stringify: function (cipherParams) {
+		// create json object with ciphertext
+		var jsonObj = {
+			ct: cipherParams.ciphertext.toString(CryptoJS.enc.Base64)
+		};
 
-function test() {
+		// optionally add iv and salt
+		if (cipherParams.iv) {
+			jsonObj.iv = cipherParams.iv.toString();
+		}
+		if (cipherParams.salt) {
+			jsonObj.s = cipherParams.salt.toString();
+		}
+
+		// stringify json object
+		return JSON.stringify(jsonObj);
+	},
+
+	parse: function (jsonStr) {
+		// parse json string
+		var jsonObj = JSON.parse(jsonStr);
+
+		// extract ciphertext from json object, and create cipher params object
+		var cipherParams = CryptoJS.lib.CipherParams.create({
+			ciphertext: CryptoJS.enc.Base64.parse(jsonObj.ct)
+		});
+
+		// optionally extract iv and salt
+		if (jsonObj.iv) {
+			cipherParams.iv = CryptoJS.enc.Hex.parse(jsonObj.iv)
+		}
+		if (jsonObj.s) {
+			cipherParams.salt = CryptoJS.enc.Hex.parse(jsonObj.s)
+		}
+
+		return cipherParams;
+	}
+};
+
+function testEnc() {
 
 	// Encrypt
-	var ciphertext = CryptoJS.AES.encrypt('my message', '123');
-	ciphertext = ciphertext.ciphertext.toString(CryptoJS.enc.Base64);
-	console.log(ciphertext);
+	var encrypted = CryptoJS.AES.encrypt("Message", "123", { format: JsonFormatter });
+	encrypted_json = encrypted.toString();
+	console.log(encrypted)
+	console.log(encrypted_json)
 
-	// Decrypt
-	//var bytes  = CryptoJS.AES.decrypt(ciphertext.toString(), '123');
-	//var plaintext = bytes.toString(CryptoJS.enc.Utf8);
-	//console.log(plaintext);
-
-	var hash = CryptoJS.SHA512(ciphertext);
-	var hash = hash.toString(CryptoJS.enc.hex);
+	hash = CryptoJS.SHA512(encrypted);
+	hash = hash.toString(CryptoJS.enc.hex);
 	console.log(hash);
 
 	$.ajax({
 		url: '/api/' + hash,
 		type: 'PUT',
-		data: ciphertext,
+		data: encrypted,
 		headers: { 'x-http-lifespan': 24*60*3 },
+		processData: false,
+		contentType: 'application/json',
 		success: function(result) {
 			console.log('success');
+			$('#dec').show();
 		}
 	});
 
+}
+
+function testDec() {
+
+	$.ajax({
+		url: '/api/' + hash,
+		type: 'GET',
+		success: function(result) {
+			console.log(result);
+			//result = encrypted_json;
+			//console.log(result);
+
+			// Decrypt
+			var decrypted = CryptoJS.AES.decrypt(result, "123", { format: JsonFormatter });
+			var decrypted_str = CryptoJS.enc.Utf8.stringify(decrypted);
+			console.log(decrypted_str);
+
+			$('#msg').html(decrypted_str);
+		}
+	});
 }
